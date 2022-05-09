95e5f81bd7068394a22fd764b3f55066e0912cb8
GabrielDemucio <GabrielDemucio@gmail.com>
2022-05-09 09:40:37 -0300

    Exercícios de criação de objetos

---
+++ src/TestaTurma.java
@@ -0,0 +1,17 @@
+
+public class TestaTurma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Turma turma = new Turma();
+		turma.periodo = "manhã";
+		turma.serie = "Segundo ano A - Informática";
+		turma.sigla = "2º AI";
+		turma.tipo = "Ensino Médio";
+		
+		System.out.println(turma.periodo + " " + turma.serie + " " + turma.sigla + " " + turma.tipo);
+		
+	}
+
+}
